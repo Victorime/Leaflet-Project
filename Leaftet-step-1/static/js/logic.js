47688a367a594e278bdeca3c8b93d2b1f656208c
Victor Ime <55962451+Victorime@users.noreply.github.com>
2020-08-07 18:43:22 -0400

    Update logic.js

--- Leaftet-step-1/static/js/logic.js
+++ Leaftet-step-1/static/js/logic.js
@@ -83,6 +83,11 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     }
   }).addTo(map);
   
+  // Here we create a legend control object.
+  var legend = L.control({
+    position: "bottomright"
+  });
+  
   
   
   
