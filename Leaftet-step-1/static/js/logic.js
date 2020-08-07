d68e421bd661ccea1a9111a5a439a8099fbc1975
Victor Ime <55962451+Victorime@users.noreply.github.com>
2020-08-07 18:33:03 -0400

    Update logic.js

--- Leaftet-step-1/static/js/logic.js
+++ Leaftet-step-1/static/js/logic.js
@@ -19,6 +19,9 @@ var map = L.map("mapid", {
   zoom: 3
 });
 
+// We then add our 'graymap' tile layer to the map.
+graymap.addTo(map);
+
 var light = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
