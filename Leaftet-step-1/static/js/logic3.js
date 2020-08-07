459c4585c2cdb4e209af772c318c7d9c91a0fb99
Victor Ime <55962451+Victorime@users.noreply.github.com>
2020-08-07 19:04:21 -0400

    Update logic3.js

--- Leaftet-step-1/static/js/logic3.js
+++ Leaftet-step-1/static/js/logic3.js
@@ -11,7 +11,7 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
   id: "mapbox.streets",
-  accessToken: API_KEY
+  accessToken: apiKey
 }).addTo(myMap);
 
 var data_url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
