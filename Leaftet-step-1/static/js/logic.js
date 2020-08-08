a0b7661841d5ff5a46208fb2693cec6936549160
Victor Ime <55962451+Victorime@users.noreply.github.com>
2020-08-07 23:12:32 -0400

    Update logic.js

--- Leaftet-step-1/static/js/logic.js
+++ Leaftet-step-1/static/js/logic.js
@@ -7,6 +7,19 @@ var QuakeMarkers=[]
 //   zoom: 3
 // });
 
+// Adding tile layer to the map
+var Street=L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+});
+
+
+
+
+
+
 
 var apiKey = API_KEY;
 
